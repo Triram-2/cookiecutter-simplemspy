bf0003fe74ea11675657bc9c3f4665ee66b3b997
Misha <bobyyy239@gmail.com>
2025-07-13 15:13:44 +0300

    chore(template): remove db settings

--- {{cookiecutter.project_slug}}/src/models/__init__.py
+++ {{cookiecutter.project_slug}}/src/models/__init__.py
@@ -1,3 +1,2 @@
 # Models package
-# All data models should inherit from Base in name.db.base
-# Ensure models are imported here or in alembic/env.py for Alembic discovery.
+# Import application models here for discovery by tooling.
